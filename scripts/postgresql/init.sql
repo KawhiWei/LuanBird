3a15916210e8bb1e3687b9b85593d81abe7a07dd
王泽威 <1585955375@qq.com>
2024-07-10 17:28:29 +0800

    feature : 初始化数据库语句

--- scripts/postgresql/init.sql
+++ scripts/postgresql/init.sql
@@ -7,7 +7,7 @@ drop schema if EXISTS shine_monitoring cascade;
 -- 创建shine_monitoring schema
 create schema shine_monitoring;
 
---#region 环境基础数据
+--#region Span
 CREATE TABLE IF NOT EXISTS span
 (
     id VARCHAR(50) NOT NULL PRIMARY KEY,
@@ -22,11 +22,104 @@ CREATE TABLE IF NOT EXISTS span
     service_instance_id VARCHAR(255) NOT NULL,
     status_message VARCHAR(1024),
     status_code INT,
-    span_kind INT NOT NULL  CHECK (duration_nanoseconds >= 0),
+    span_kind INT NOT NULL  CHECK (span_kind >= 0),
     trace_flags INT NOT NULL CHECK (trace_flags >= 0),
     trace_state VARCHAR(1024),
     );
 
-CREATE INDEX idx_shine_monitoring_span_id on span (span_id);
-CREATE INDEX idx_shine_monitoring_trace_id on span (trace_id);
+CREATE INDEX idx_shine_monitoring_span_span_id on span (span_id);
+CREATE INDEX idx_shine_monitoring_span_trace_id on span (trace_id);
 --#endregion
+
+--#region SpanAttribute
+CREATE TABLE IF NOT EXISTS span_attribute
+(
+    id VARCHAR(50) NOT NULL PRIMARY KEY,
+    trace_id VARCHAR(255) NOT NULL,
+    span_id VARCHAR(255) NOT NULL,
+    `key` VARCHAR(255) NOT NULL,
+    value_type INT NOT NULL  CHECK (value_type >= 0),
+    `value` VARCHAR(255) NOT NULL,
+    );
+
+CREATE INDEX idx_shine_monitoring_span_attribute_span_id on span_attribute (span_id);
+CREATE INDEX idx_shine_monitoring_span_attribute_trace_id on span_attribute (trace_id);
+--#endregion
+
+--#region ResourceAttribute
+CREATE TABLE IF NOT EXISTS resource_attribute
+(
+    id VARCHAR(50) NOT NULL PRIMARY KEY,
+    trace_id VARCHAR(255) NOT NULL,
+    span_id VARCHAR(255) NOT NULL,
+    `key` VARCHAR(255) NOT NULL,
+    value_type INT NOT NULL  CHECK (value_type >= 0),
+    `value` VARCHAR(255) NOT NULL,
+    );
+
+CREATE INDEX idx_shine_monitoring_resource_attribute_span_id on resource_attribute (span_id);
+CREATE INDEX idx_shine_monitoring_resource_attribute_trace_id on resource_attribute (trace_id);
+--#endregion
+
+--#region SpanEvent
+CREATE TABLE IF NOT EXISTS span_event
+(
+    id VARCHAR(50) NOT NULL PRIMARY KEY,
+    trace_id VARCHAR(255) NOT NULL,
+    span_id VARCHAR(255) NOT NULL,
+    `index` int NOT NULL CHECK (value_type >= 0),
+    `name` VARCHAR(255) NOT NULL,
+    timestamp_unix_nano BIGINT NOT NULL CHECK (timestamp_unix_nano >= 0),
+    );
+
+CREATE INDEX idx_shine_monitoring_span_event_span_id on span_event (span_id);
+CREATE INDEX idx_shine_monitoring_span_event_trace_id on span_event (trace_id);
+--#endregion
+
+--#region SpanEventAttribute
+CREATE TABLE IF NOT EXISTS span_event_attribute
+(
+    id VARCHAR(50) NOT NULL PRIMARY KEY,
+    trace_id VARCHAR(255) NOT NULL,
+    span_event_index int NOT NULL CHECK (span_event_index >= 0),
+    span_id VARCHAR(255) NOT NULL,
+    `key` VARCHAR(255) NOT NULL,
+    value_type INT NOT NULL  CHECK (value_type >= 0),
+    `value` VARCHAR(255) NOT NULL,
+    );
+
+CREATE INDEX idx_shine_monitoring_span_event_attribute_span_id on span_event_attribute (span_id);
+CREATE INDEX idx_shine_monitoring_span_event_attribute_trace_id on span_event_attribute (trace_id);
+--#endregion
+
+--#region SpanLink
+CREATE TABLE IF NOT EXISTS span_link
+(
+    id VARCHAR(50) NOT NULL PRIMARY KEY,
+    trace_id VARCHAR(255) NOT NULL,
+    span_id VARCHAR(255) NOT NULL,
+    `index` int NOT NULL CHECK (value_type >= 0),
+    linked_trace_id VARCHAR(255) NOT NULL,
+    linked_span_id VARCHAR(255) NOT NULL,
+    linked_trace_state VARCHAR(1024),
+    linked_trace_flags INT CHECK (linked_trace_flags >= 0),
+    );
+
+CREATE INDEX idx_shine_monitoring_span_link_span_id on span_link (span_id);
+CREATE INDEX idx_shine_monitoring_span_link_trace_id on span_link (trace_id);
+--#endregion
+
+--#region SpanLinkAttribute
+CREATE TABLE IF NOT EXISTS span_link_attribute
+(
+    id VARCHAR(50) NOT NULL PRIMARY KEY,
+    trace_id VARCHAR(255) NOT NULL,
+    span_link_index int NOT NULL CHECK (span_event_index >= 0),
+    span_id VARCHAR(255) NOT NULL,
+    `key` VARCHAR(255) NOT NULL,
+    value_type INT NOT NULL  CHECK (value_type >= 0),
+    `value` VARCHAR(255) NOT NULL,
+    );
+
+CREATE INDEX idx_shine_monitoring_span_link_attribute_span_id on span_link (span_id);
+CREATE INDEX idx_shine_monitoring_span_link_attribute_trace_id on span_link (trace_id);
