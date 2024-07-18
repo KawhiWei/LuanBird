112e33db01e4c202a7e0c311d185bb4f209f4f77
王泽威 <1585955375@qq.com>
2024-07-18 20:40:16 +0800

    feature： 添加日志打印

--- src/Shine.Receiver/Services/OTelTraceReceiverService.cs
+++ src/Shine.Receiver/Services/OTelTraceReceiverService.cs
@@ -1,10 +1,11 @@
 using Grpc.Core;
+using Luck.Framework.Extensions;
 using OpenTelemetry.Proto.Collector.Trace.V1;
 using Shine.Receiver.Extensions;
 
 namespace Shine.Receiver.Services;
 
-public class OTelTraceReceiverService : TraceService.TraceServiceBase
+public class OTelTraceReceiverService(ILogger<OTelTraceReceiverService> logger) : TraceService.TraceServiceBase
 {
     public override async Task<ExportTraceServiceResponse> Export(ExportTraceServiceRequest request,
         ServerCallContext context)
@@ -13,6 +14,7 @@ public class OTelTraceReceiverService : TraceService.TraceServiceBase
         var shineSpans = request.ResourceSpans
             .SelectMany(resourceSpans => resourceSpans.ScopeSpans
                 .SelectMany(scopeSpans => scopeSpans.Spans.Select(span => span.ToShineSpan(resourceSpans.Resource))));
+        logger.LogInformation($"提交的请求数据：【{shineSpans.Serialize()}】");
         return new ExportTraceServiceResponse();
     }
 }
